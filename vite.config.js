64b26fb28be4042e2249749a2a67a970a5f40dd4
Anthony Famorcan <anthony25famorcan@gmail.com>
2024-09-11 10:36:08 +0800

    Fix vite issues on dev environment

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', // Allows Vite to be accessed from outside the container
+        port: 5173,      // Default port
+        hmr: {
+            host: 'localhost', // HMR should point to the host
+            protocol: 'ws',    // Use WebSockets for HMR
+            port: 5173,        // Port for HMR (same as server port)
+        },
+    },
 });
