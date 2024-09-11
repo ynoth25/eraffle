701275a412c7f50b3e9ffd8a7d19a8bf5f1ed690
Anthony Famorcan <anthony25famorcan@gmail.com>
2024-09-11 10:43:47 +0800

    Develop (#7)
    * Add basic user-authentication
    
    * Fix vite issues on dev environment (#6)
    
    ---------
    
    Co-authored-by: Akzayado <54758286+Akzayado@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', // Allows Vite to be accessed from outside the container
+        port: 5173,      // Default port
+        hmr: {
+            host: 'localhost', // HMR should point to the host
+            protocol: 'ws',    // Use WebSockets for HMR
+            port: 5173,        // Port for HMR (same as server port)
+        },
+    },
 });
