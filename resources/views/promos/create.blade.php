83afb2fa3686cb5420f115fcfdaab16ff1ccbbde
Anthony <anthony25famorcan@gmail.com>
2024-09-18 12:15:59 +0800

    Add raffle entry function

--- resources/views/promos/create.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- resources/views/promos/create.blade.php -->
-@extends('layouts.app')
-
-@section('content')
-    <h1>Create Promo</h1>
-
-    @include('promos.form', ['promo' => null])
-@endsection
--- resources/views/promos/edit.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- resources/views/promos/edit.blade.php -->
-@extends('layouts.app')
-
-@section('content')
-    <h1>Edit Promo</h1>
-
-    @include('promos.form', ['promo' => $promo])
-@endsection
